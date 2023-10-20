83da830cfb742f236b05c1f47f0695c80892d02a
elishagretton <elishagretton@gmail.com>
2023-10-20 11:19:43 +0100

    Adds milestone_3.py file

---
+++ milestone_3.py
@@ -0,0 +1,26 @@
+import random
+
+word_list = ['apple','banana','grapes','strawberries','kiwis']
+word = random.choice(word_list)
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+def ask_for_input():
+
+    # Get input in correct form
+    while True:
+        guess = input('Guess a letter: ')
+        if len(guess)==1 and guess.isalpha():
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+   
+    # Retrieved valid input, now check guess
+    check_guess(guess)
+
+ask_for_input()
